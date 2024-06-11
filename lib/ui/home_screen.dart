c857baf027f9cb8839aa41ec5322a1cc0d08948a
sumitsirsi58 <sumitsirsi58@gmail.com>
2024-06-11 18:37:01 +0530

    Add drawer and search bar

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:codexias_academy/ui/student_full_detail_screen.dart';
 import 'package:codexias_academy/widget/student_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import '../model/student_moel.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -33,17 +34,113 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          const Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(Icons.notifications),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.blue.shade50,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SafeArea(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AddStudentScreen(),
+                        ));
+                  },
+                  child: const ListTile(
+                    leading: Icon(
+                      Icons.add,
+                      size: 30,
+                      color: Colors.black54,
+                    ),
+                    title: Text(
+                      'Add Student',
+                      style: TextStyle(color: Colors.black54, fontSize: 20),
+                    ),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.note,
+                    size: 30,
+                    color: Colors.black54,
+                  ),
+                  title: Text(
+                    'Notes',
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    size: 30,
+                    color: Colors.black54,
+                  ),
+                  title: Text(
+                    'Setting',
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.privacy_tip_sharp,
+                    size: 30,
+                    color: Colors.black54,
+                  ),
+                  title: Text(
+                    'Privacy',
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.help_outline,
+                    size: 30,
+                    color: Colors.black54,
+                  ),
+                  title: Text(
+                    'Help',
+                    style: TextStyle(color: Colors.black54, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => AddStudentScreen(),
+                builder: (context) => const AddStudentScreen(),
               ));
           ;
         },
         backgroundColor: ColorConst.floatingButton,
-        child: Icon(
+        child: const Icon(
           Icons.add,
           color: Colors.white,
         ),
@@ -56,19 +153,52 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
+                  Container(
+                    height: 48,
+                    child: TextField(
+                      cursorColor: Colors.grey,
+                      cursorHeight: 20,
+                      decoration: InputDecoration(
+                          label: const Text(
+                            'Search.....',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade100,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(
+                                color: Colors.grey.shade200,
+                              ))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
                   GestureDetector(
-                    onTap: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => StudentFullScreen(studentModel: std),));
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                StudentFullScreen(studentModel: std),
+                          ));
                     },
                     child: Card(
-                      elevation: 20,
+                      elevation: 8,
                       child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue.shade50,
-                        borderRadius: BorderRadius.circular(14)
-                      ),
-
-                        child:StudentCardWidget(std: std),
+                        decoration: BoxDecoration(
+                            color: Colors.blue.shade50,
+                            borderRadius: BorderRadius.circular(14)),
+                        child: StudentCardWidget(std: std),
                       ),
                     ),
                   )
