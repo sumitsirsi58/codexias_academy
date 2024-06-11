5a1464fbbc06dacbae27740ac08bb4f86606d08d
sumitsirsi58 <sumitsirsi58@gmail.com>
2024-06-11 18:38:39 +0530

    add pay scrren and chat screen

--- lib/ui/student_full_detail_screen.dart
+++ lib/ui/student_full_detail_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+import 'package:codexias_academy/const/color_const.dart';
 import 'package:codexias_academy/model/student_moel.dart';
+import 'package:codexias_academy/ui/pay_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StudentFullScreen extends StatefulWidget {
   const StudentFullScreen({required this.studentModel, super.key});
@@ -16,177 +19,219 @@ class _StudentFullScreenState extends State<StudentFullScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                height: 120,
-                width: 120,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(80),
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey.shade200)),
-                child: CircleAvatar(
-                  radius: 32,
-                  backgroundImage: widget.studentModel.image != null
-                      ? FileImage(File(widget.studentModel.image!))
-                      : const AssetImage('assets/images/scooter_2289821.png') as ImageProvider,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Card(
-              elevation: 20,
-              child: Container(
-                //height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14),
-                  border: Border.all(color: Colors.blue.shade50),
-                  color: Colors.blue.shade50,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              nameText('Name'),
-                              buildSizedBox(),
-                              nameText('LastName'),
-                              buildSizedBox(),
-                              nameText('FatherName'),
-                              buildSizedBox(),
-                              nameText('Village'),
-                              buildSizedBox(),
-                              nameText('Fess'),
-                              buildSizedBox(),
-                              nameText('MobileNo'),
-                              buildSizedBox(),
-                              nameText('JoinDate'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                              buildSizedBox(),
-                              dotText(),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              buildText(widget.studentModel.name),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.lastName),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.fatherName),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.village),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.fees),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.mobileNo),
-                              buildSizedBox(),
-                              buildText(widget.studentModel.joinDate),
-                            ],
-                          )
-                        ],
-                      ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      //   children: [
-                      //
-                      //
-                      //
-                      //   ],
-                      // ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      //   children: [
-                      //
-                      //     Text(':'),
-                      //     Text('${widget.studentModel.lastName}'),
-                      //   ],
-                      // ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      //   children: [
-                      //
-                      //     Text(':'),
-                      //     Text('${widget.studentModel.fatherName}'),
-                      //   ],
-                      // ),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      //   children: [
-                      //
-                      //     Text(':'),
-                      //     Text('${widget.studentModel.village}'),
-                      //   ],
-                      // ),
-                      //
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      //
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      // Text(
-                      //     'FatherName         :          ${widget.studentModel.fatherName}'),
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      // Text(
-                      //     'Village                  :           ${widget.studentModel.village}'),
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      // Text(
-                      //     'Fees                      :          ${widget.studentModel.fees}'),
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      // Text(
-                      //     'MobileNo             :          ${widget.studentModel.mobileNo}'),
-                      // SizedBox(
-                      //   height: 4,
-                      // ),
-                      // Text(
-                      //     'JoniDate               :         ${widget.studentModel.joinDate}'),
-                    ],
+        child: SafeArea(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey.shade200)),
+                  child: CircleAvatar(
+                    radius: 32,
+                    backgroundImage: widget.studentModel.image != null
+                        ? FileImage(File(widget.studentModel.image!))
+                        : const AssetImage('assets/images/scooter_2289821.png')
+                            as ImageProvider,
                   ),
                 ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Card(
+                elevation: 8,
+                child: Container(
+                  //height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    border: Border.all(color: Colors.blue.shade50),
+                    color: Colors.blue.shade50,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                nameText('Name'),
+                                buildSizedBox(),
+                                nameText('LastName'),
+                                buildSizedBox(),
+                                nameText('FatherName'),
+                                buildSizedBox(),
+                                nameText('Village'),
+                                buildSizedBox(),
+                                nameText('MobileNo'),
+                                buildSizedBox(),
+                                nameText('JoinDate'),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                                buildSizedBox(),
+                                dotText(),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                buildText(widget.studentModel.name),
+                                buildSizedBox(),
+                                buildText(widget.studentModel.lastName),
+                                buildSizedBox(),
+                                buildText(widget.studentModel.fatherName),
+                                buildSizedBox(),
+                                buildText(widget.studentModel.village),
+                                buildSizedBox(),
+                                buildText(widget.studentModel.mobileNo),
+                                buildSizedBox(),
+                                buildText(widget.studentModel.joinDate),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                children: [
+                  Card(
+                    elevation: 8,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        border: Border.all(color: Colors.blue.shade50),
+                        color: Colors.blue.shade50,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    nameText('TotalFees'),
+                                    buildSizedBox(),
+                                    nameText('PayFees'),
+                                    buildSizedBox(),
+                                    nameText('PendingFees'),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    dotText(),
+                                    buildSizedBox(),
+                                    dotText(),
+                                    buildSizedBox(),
+                                    dotText(),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    buildText(widget.studentModel.totalFees),
+                                    buildSizedBox(),
+                                    buildText(widget.studentModel.payFees),
+                                    buildSizedBox(),
+                                    buildText(widget.studentModel.pendingFees),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PaYScreen(),
+                            ));
+                      },
+                      child: Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            color: ColorConst.buttonColor),
+                        child: const Center(
+                            child: Text(
+                          'Pay Fees',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14),
+                          color: ColorConst.buttonColor),
+                      child: const Center(
+                          child: Text(
+                        'Chat',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  SizedBox buildSizedBox() => SizedBox(height: 4);
+  SizedBox buildSizedBox() => const SizedBox(height: 4);
 
   Text nameText(String text) => Text(text);
 
